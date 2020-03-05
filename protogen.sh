7a5cd8e1554c9d20eac530fd3d19e7cf28edaa7e
hitman <kabalx47@gmail.com>
2020-03-05 23:26:34 +0200

    c

--- protogen.sh
+++ protogen.sh
@@ -13,6 +13,16 @@ protoc \
     --grpc_out="${OUT_DIR}" \
     graph.proto
 
+OUT_DIR="./proxy/src/generated"
+protoc \
+    --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
+    --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+    --js_out=import_style=commonjs,binary:${OUT_DIR} \
+    --ts_out=service=grpc-node:${OUT_DIR} \
+    --grpc_out="${OUT_DIR}" \
+    graph.proto
+
+
 OUT_DIR="./client/src/generated"
 protoc \
   --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
