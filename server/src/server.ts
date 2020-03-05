ea4949cb6511e94e41b804d38fb5a02717624a6b
hitman <kabalx47@gmail.com>
2020-03-05 08:32:29 +0200

    server template

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,19 @@
-import * as messages from "./generated/hello_pb";
+import { GraphClient } from "./generated/graph_grpc_pb";
+import { Node } from "./generated/graph_pb";
+import { Server, ServerCredentials } from "grpc";
+import { config } from "dotenv";
 
-console.log(messages);
+function createServer() {
+  const server = new Server();
+  return server;
+}
+
+if (!module.parent) {
+  config();
+  const server = createServer();
+  const port = process.env.PORT || 3000;
+  //   server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
+  server.bind(`0.0.0.0:${port}`, ServerCredentials.createInsecure());
+  global.console.log(`Server binded to 0.0.0.0:${port}`);
+  server.start();
+}
