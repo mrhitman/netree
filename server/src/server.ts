4e9c1285f1940978d1e8c0403a0e797d5701e143
Varenik Victor <kabalx47@gmail.com>
2020-03-06 19:50:44 +0200

    -

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,27 @@
-import { config } from "dotenv";
-import { Server, ServerCredentials } from "grpc";
-import { GraphFileDataProvider } from "./components/graph-data-provider";
-import * as graph from "./generated/graph_grpc_pb.js";
-import NodeDefinition from "./implementations/node-service";
+import { config } from 'dotenv';
+import { Server, ServerCredentials, loadPackageDefinition } from 'grpc';
+import { GraphFileDataProvider } from './components/graph-data-provider';
+import * as graph from './generated/graph_grpc_pb.js';
+import * as protoLoader from '@grpc/proto-loader';
+import NodeDefinition from './implementations/node-service';
 
 config();
 
 export function createServer() {
-  const server = new Server();
-  const implementation = new NodeDefinition(
-    new GraphFileDataProvider("data.txt")
-  );
+	const packageDefinition = protoLoader.loadSync('../graph.proto');
+	const graphProto = loadPackageDefinition(packageDefinition).api as any;
 
-  const service = (graph as any)["api.Graph"];
-  server.addService(service, implementation);
-  return server;
+	const server = new Server();
+	const implementation = new NodeDefinition(new GraphFileDataProvider('data.txt'));
+
+	server.addService<graph.IGraphService>(graph.GraphService, implementation);
+	return server;
 }
 
 if (!module.parent) {
-  const port = process.env.PORT || 3000;
-  const server = createServer();
-  server.bind(`0.0.0.0:${port}`, ServerCredentials.createInsecure());
-  server.start();
-  global.console.log(`Server binded to 0.0.0.0:${port}`);
+	const port = process.env.PORT || 3000;
+	const server = createServer();
+	server.bind(`0.0.0.0:${port}`, ServerCredentials.createInsecure());
+	server.start();
+	global.console.log(`Server binded to 0.0.0.0:${port}`);
 }
