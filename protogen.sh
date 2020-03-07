93de08cabb65acc87d96f4a9d3ccda894f0a2da5
hitman <kabalx47@gmail.com>
2020-03-07 16:58:58 +0200

    pretty protogen script

--- protogen.sh
+++ protogen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PROTOC="./node_modules/.bin/grpc_tools_node_protoc"
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
 OUT_DIR="./generated"
@@ -9,22 +10,20 @@ echo "Compiling protobuf definitions"
 rm -rf ${OUT_DIR}
 mkdir ${OUT_DIR}
 
-
-./node_modules/.bin/grpc_tools_node_protoc \
-  --js_out=import_style=commonjs,binary:./generated \
-  --grpc_out='./generated' \
-  --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-  -I ./generated/ \
+${PROTOC} \
+  --js_out=import_style=commonjs,binary:${OUT_DIR} \
+  --grpc_out=${OUT_DIR} \
+  --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+  -I ${OUT_DIR} \
   -I ./ \
  *.proto
 
-./node_modules/.bin/grpc_tools_node_protoc \
-  --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-  --ts_out=./generated \
-  -I ./generated \
+${PROTOC} \
+  --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
+  --ts_out=${OUT_DIR} \
+  -I ${OUT_DIR} \
   -I ./ \
  *.proto
-
 
  rm -rf ./server/src/generated
  rm -rf ./client/src/generated
